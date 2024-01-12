083e8b461079711885e32e87b8b662a2dcb93c4f
Jinyong Guo <macopad@qq.com>
2024-01-13 00:20:56 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/ryanrolds/sqlclosecheck/pkg/analyzer"
+	"github.com/macopad/sqltransactioncheck/pkg/analyzer"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
